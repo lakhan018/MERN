6e8ade1dc348abb140f8c1d6a87ed7d7afcf1fb2
Dushant Pariskar <dushantpariskar@gmail.com>
2025-07-28 10:11:01 +0530

    du

--- middlewareGlobalCatch3.1/index.js
+++ middlewareGlobalCatch3.1/index.js
@@ -1,11 +1,27 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const kidney = req.query.kidney;
 
-    const username = req.headers.username;
-    const password = req.headers.password;
+  if (!(username === "harkirat" && password === "pass")) {
+      res.status(400).json({ msg: "wrong username password" });
 
-    if(username==='harkirat' && password==='pass'){
-        res.send("you can acess data ");
-    }
-});app.listen(3000);
+    return;
+  } else {
+    // console.log(typeof(kidney))
+    if (!(kidney == 1 && kidney == 2)) {
+      res.status(400).json({ msg: "kidney check" });
+      return;
+    }res.json({"msg":"you can  access the data"})
+  }
+});
+
+app.put("/",(req,res)=>{
+
+    kidney=1;
+    res.json({"msg":"done repalce kidney count to 1"})
+
+})
+app.listen(3000);
